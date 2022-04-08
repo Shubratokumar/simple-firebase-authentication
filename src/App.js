02932ab8d884d1fe4f7c1f48a13819777e056f16
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-08 11:39:39 +0600

    google sign method added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
-
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState()
 
+  const googleProvider = new GoogleAuthProvider();
   const handleGoogleSignIn = () =>{
-    signInWithPopup(auth, provider)
-    .then(result => {
+    signInWithPopup(auth, googleProvider)
+    .then((result) =>{
       const user = result.user;
-      console.log(user);
+      setUser(user)
     })
-    .catch(error => {
-      console.log(error);
+    .catch((error) =>{
+      console.error('error', error)
     })
   }
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then(() =>{
+      setUser({})
+    })
+    .catch((error) =>{
+      setUser({})
+    })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
-      
+      {
+        !user.email ? <button onClick={handleGoogleSignIn}>Google Sign In</button> : <button onClick={handleSignOut}>Sign Out</button>
+      }
+        <h2>Name : {user.displayName}</h2>
+        <p>I know your email address : {user.email}</p>
+        <img src={user.photoURL} alt="" />
     </div>
   );
 }
