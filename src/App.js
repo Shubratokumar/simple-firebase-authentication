482ba6929cbe868faa2f16e110b1fb1451e8f481
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-09 15:20:38 +0600

    facebook sign in added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
+  FacebookAuthProvider,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
@@ -23,22 +24,35 @@ function App() {
         setUser(user);
       })
       .catch((error) => {
-        console.error("error", error);
+        console.error(error);
       });
   };
 
   const gitHubProvider = new GithubAuthProvider();
 
-  const handleGitHubSignIn = () =>{
+  const handleGitHubSignIn = () => {
     signInWithPopup(auth, gitHubProvider)
-    .then((result) =>{
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const faceBookProvider = new FacebookAuthProvider();
+
+  const handleFacebookSignIn = () =>{
+    signInWithPopup(auth, faceBookProvider)
+    .then((result) => {
       const user = result.user;
+      console.log(user)
       setUser(user)
     })
     .catch((error) =>{
       console.error(error)
     })
-
   }
   const handleSignOut = () => {
     signOut(auth)
@@ -53,19 +67,20 @@ function App() {
   return (
     <div className="App">
       {/* <button onClick={handleGoogleSignIn}>Google Sign In</button>
-      <button onClick={handleGitHubSignIn}>GitHub Sign In</button> */}
-      {
-        !user.photoURL ? 
+      <button onClick={handleGitHubSignIn}>GitHub Sign In</button>
+      <button onClick={handleFacebookSignIn}>Facebook Login</button> */}
+      {user?.uid ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
         <>
           <button onClick={handleGoogleSignIn}>Google Sign In</button>
           <button onClick={handleGitHubSignIn}>GitHub Sign In</button>
+          <button onClick={handleFacebookSignIn}>Facebook Login</button>
         </>
-       :  <button onClick={handleSignOut}>Sign Out</button>
-        
-      }
-      <h2>Name : {user.displayName}</h2>
-      <p>I know your email address : {user.email}</p>
-      <img src={user.photoURL} alt="" />
+      )}
+      <h2>Name : {user?.displayName}</h2>
+      <p>I know your email address : {user?.email}</p>
+      <img src={user?.photoURL} alt="" />
     </div>
   );
 }
